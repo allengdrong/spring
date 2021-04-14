8e608e265228163f79a5c7cb073080f61fc03734
allengdrong <bluesky0232@naver.com>
2021-04-14 16:34:00 +0900

    spring project study

---
+++ ex01/src/test/java/org/zerock/board/mapper/BoardMapperTests.java
@@ -0,0 +1,37 @@
+package org.zerock.board.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.board.vo.BoardVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTests {
+	
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+	
+	@Test
+	public void testList() {
+		log.info(mapper.list());
+	}
+	
+	@Test
+	public void testWrite() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("test title");
+		vo.setContent("test content");
+		vo.setWriter("test");
+		vo.setPw("1111");
+		
+		log.info(mapper.write(vo));
+	}
+
+}
